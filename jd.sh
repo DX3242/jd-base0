a4abff42048ce0355641d844df1b1440444df464
ZFeng3242 <pl8354629pou@163.com>
2021-08-09 23:01:30 +0800

    新增手机狂欢城互助变量

--- jd.sh
+++ jd.sh
@@ -95,8 +95,7 @@ function Combin_All {
   export JDCFD_SHARECODES=$(Combin_Sub ForOtherCfd)
   export JDHEALTH_SHARECODES=$(Combin_Sub ForOtherhealth)
   export CITY_SHARECODES=$(Combin_Sub ForOtherCity)
-  export JDZOO_SHARECODES=$(Combin_Sub ForOtherZoo)
-  export JDSUMMERSH_SHARECODES=$(Combin_Sub ForOtherSummer)
+  export JD818_SHARECODES=$(Combin_Sub ForOtherJD818)
 }
 
 ## 转换JD_BEAN_SIGN_STOP_NOTIFY或JD_BEAN_SIGN_NOTIFY_SIMPLE
