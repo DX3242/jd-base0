54c35a64c5ab65e06c48467120d417999299a8e0
ZFeng3242 <pl8354629pou@163.com>
2021-11-03 01:03:58 +0800

    test

--- jd.sh
+++ jd.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
 ## 路径
-ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+ShellDir=${JD_DIR:-$(
+  cd $(dirname $0)
+  pwd
+)}
 [ ${JD_DIR} ] && HelpJd=jd || HelpJd=jd.sh
 ScriptsDir=${ShellDir}/scripts
 ConfigDir=${ShellDir}/config
 FileConf=${ConfigDir}/config.sh
 FileConfSample=${ShellDir}/sample/config.sh.sample
 LogDir=${ShellDir}/log
-ListScripts=($(cd ${ScriptsDir}; ls *.js | grep -E "j[drx]_"))
+ACCOUNT_PATH=/usr/share/nginx/html/JDAccount.json
+TMP_CONFIGFILE=/jd/config/tmpConfig.sh
+WEB_PATH=/usr/share/nginx/html/
+ListScripts=($(
+  cd ${ScriptsDir}
+  ls *.js | grep -E "j[drx]_"
+))
 ListCron=${ConfigDir}/crontab.list
 
 ## 导入config.sh
-function Import_Conf {
-  if [ -f ${FileConf} ]
-  then
+function Import_Conf() {
+  if [ -f ${FileConf} ]; then
     . ${FileConf}
     if [ -z "${Cookie1}" ]; then
       echo -e "请先在config.sh中配置好Cookie...\n"
@@ -27,15 +35,15 @@ function Import_Conf {
 }
 
 ## 更新crontab
-function Detect_Cron {
+function Detect_Cron() {
   if [[ $(cat ${ListCron}) != $(crontab -l) ]]; then
     crontab ${ListCron}
   fi
 }
 
 ## 用户数量UserSum
-function Count_UserSum {
-  for ((i=1; i<=1000; i++)); do
+function Count_UserSum() {
+  for ((i = 1; i <= 1000; i++)); do
     Tmp=Cookie$i
     CookieTmp=${!Tmp}
     [[ ${CookieTmp} ]] && UserSum=$i || break
@@ -43,20 +51,20 @@ function Count_UserSum {
 }
 
 ## 组合Cookie和互助码子程序
-function Combin_Sub {
+function Combin_Sub() {
   CombinAll=""
   if [[ ${AutoHelpOther} == true ]] && [[ $1 == ForOther* ]]; then
 
     ForOtherAll=""
     MyName=$(echo $1 | perl -pe "s|ForOther|My|")
 
-    for ((m=1; m<=${UserSum}; m++)); do
+    for ((m = 1; m <= ${UserSum}; m++)); do
       TmpA=${MyName}$m
       TmpB=${!TmpA}
       ForOtherAll="${ForOtherAll}@${TmpB}"
     done
 
-    for ((n=1; n<=${UserSum}; n++)); do
+    for ((n = 1; n <= ${UserSum}; n++)); do
       for num in ${TempBlockCookie}; do
         [[ $n -eq $num ]] && continue 2
       done
@@ -64,7 +72,7 @@ function Combin_Sub {
     done
 
   else
-    for ((i=1; i<=${UserSum}; i++)); do
+    for ((i = 1; i <= ${UserSum}; i++)); do
       for num in ${TempBlockCookie}; do
         [[ $i -eq $num ]] && continue 2
       done
@@ -78,7 +86,7 @@ function Combin_Sub {
 }
 
 ## 组合Cookie、Token与互助码
-function Combin_All {
+function Combin_All() {
   export JD_COOKIE=$(Combin_Sub Cookie)
   export FRUITSHARECODES=$(Combin_Sub ForOtherFruit)
   export PETSHARECODES=$(Combin_Sub ForOtherPet)
@@ -100,24 +108,24 @@ function Combin_All {
 }
 
 ## 转换JD_BEAN_SIGN_STOP_NOTIFY或JD_BEAN_SIGN_NOTIFY_SIMPLE
-function Trans_JD_BEAN_SIGN_NOTIFY {
+function Trans_JD_BEAN_SIGN_NOTIFY() {
   case ${NotifyBeanSign} in
-    0)
-      export JD_BEAN_SIGN_STOP_NOTIFY="true"
-      ;;
-    1)
-      export JD_BEAN_SIGN_NOTIFY_SIMPLE="true"
-      ;;
+  0)
+    export JD_BEAN_SIGN_STOP_NOTIFY="true"
+    ;;
+  1)
+    export JD_BEAN_SIGN_NOTIFY_SIMPLE="true"
+    ;;
   esac
 }
 
 ## 转换UN_SUBSCRIBES
-function Trans_UN_SUBSCRIBES {
+function Trans_UN_SUBSCRIBES() {
   export UN_SUBSCRIBES="${goodPageSize}\n${shopPageSize}\n${jdUnsubscribeStopGoods}\n${jdUnsubscribeStopShop}"
 }
 
 ## 申明全部变量
-function Set_Env {
+function Set_Env() {
   Count_UserSum
   Combin_All
   Trans_JD_BEAN_SIGN_NOTIFY
@@ -125,7 +133,7 @@ function Set_Env {
 }
 
 ## 随机延迟
-function Random_Delay {
+function Random_Delay() {
   if [[ -n ${RandomDelay} ]] && [[ ${RandomDelay} -gt 0 ]]; then
     CurMin=$(date "+%-M")
     if [[ ${CurMin} -gt 2 && ${CurMin} -lt 30 ]] || [[ ${CurMin} -gt 31 && ${CurMin} -lt 59 ]]; then
@@ -137,7 +145,7 @@ function Random_Delay {
 }
 
 ## 使用说明
-function Help {
+function Help() {
   echo -e "本脚本的用法为："
   echo -e "1. bash ${HelpJd} xxx      # 如果设置了随机延迟并且当时时间不在0-2、30-31、59分内，将随机延迟一定秒数"
   echo -e "2. bash ${HelpJd} xxx now  # 无论是否设置了随机延迟，均立即运行"
@@ -146,25 +154,25 @@ function Help {
   echo -e "\n针对用法1、用法2中的\"xxx\"，可以不输入后缀\".js\"，另外，如果前缀是\"jd_\"的话前缀也可以省略。"
   echo -e "当前有以下脚本可以运行（仅列出以jd_、jr_、jx_开头的脚本）："
   cd ${ScriptsDir}
-  for ((i=0; i<${#ListScripts[*]}; i++)); do
+  for ((i = 0; i < ${#ListScripts[*]}; i++)); do
     Name=$(grep "new Env" ${ListScripts[i]} | awk -F "'|\"" '{print $2}')
     echo -e "$(($i + 1)).${Name}：${ListScripts[i]}"
   done
 }
 
 ## nohup
-function Run_Nohup {
+function Run_Nohup() {
   if [[ $(ps -ef | grep "${js}" | grep -v "grep") != "" ]]; then
     ps -ef | grep "${js}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
   fi
   [ ! -d ${LogDir}/${js} ] && mkdir -p ${LogDir}/${js}
   LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
   LogFile="${LogDir}/${js}/${LogTime}.log"
-  nohup node ${js}.js > ${LogFile} &
+  nohup node ${js}.js >${LogFile} &
 }
 
 ## 运行挂机脚本
-function Run_HangUp {
+function Run_HangUp() {
   HangUpJs=""
   cd ${ScriptsDir}
   for js in ${HangUpJs}; do
@@ -180,13 +188,13 @@ function Run_HangUp {
 }
 
 ## 重置密码
-function Reset_Pwd {
+function Reset_Pwd() {
   cp -f ${ShellDir}/sample/auth.json ${ConfigDir}/auth.json
   echo -e "控制面板重置成功，用户名：admin，密码：shuye72\n"
 }
 
 ## 运行脚本
-function Run_Normal {
+function Run_Normal() {
   Import_Conf $1 && Detect_Cron && Set_Env
 
   FileNameTmp1=$(echo $1 | perl -pe "s|\.js||")
@@ -195,8 +203,7 @@ function Run_Normal {
   FileName=""
   WhichDir=""
 
-  for dir in ${SeekDir}
-  do
+  for dir in ${SeekDir}; do
     if [ -f ${dir}/${FileNameTmp1}.js ]; then
       FileName=${FileNameTmp1}
       WhichDir=${dir}
@@ -208,8 +215,7 @@ function Run_Normal {
     fi
   done
 
-  if [ -n "${FileName}" ] && [ -n "${WhichDir}" ]
-  then
+  if [ -n "${FileName}" ] && [ -n "${WhichDir}" ]; then
     [ $# -eq 1 ] && Random_Delay
     LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
     LogFile="${LogDir}/${FileName}/${LogTime}.log"
@@ -224,38 +230,77 @@ function Run_Normal {
 
 ## 命令检测
 case $# in
-  0)
-    echo
-    Help
-    ;;
-  1)
-    if [[ $1 == hangup ]]; then
-      Run_HangUp
-    elif [[ $1 == resetpwd ]]; then
-      Reset_Pwd
+0)
+  echo
+  Help
+  ;;
+1)
+  if [[ $1 == hangup ]]; then
+    Run_HangUp
+  elif [[ $1 == resetpwd ]]; then
+    Reset_Pwd
+  else
+    if [ -x "$(command -v jq)" ] && [ -f $ACCOUNT_PATH ]; then
+      USERS=$(jq -r "to_entries|map(\"\(.key)\")|.[]" $ACCOUNT_PATH)
+      for USER in $USERS; do
+        for i in {1..50}; do
+          unset 'Cookie'$i
+        done
+        cd $WEB_PATH &&
+          php -r "require '${WEB_PATH}/functions.php';replaceCookieInShell('${USER}');updateWorkWechatConfig('${USER}');" &&
+          cd /jd
+        FileConf=$TMP_CONFIGFILE
+        Run_Normal $1 1
+      done
+      cd $WEB_PATH &&
+        php -r "require '${WEB_PATH}/functions.php';getUnBindUsers();updateWorkWechatConfig();" &&
+        cd /jd
+      FileConf=$TMP_CONFIGFILE
+      Run_Normal $1 1
     else
       Run_Normal $1
     fi
-    ;;
-  2)
-    if [[ $2 == now ]]; then
+
+  fi
+  ;;
+2)
+  if [[ $2 == now ]]; then
+    if [ -x "$(command -v jq)" ] && [ -f $ACCOUNT_PATH ]; then
+      USERS=$(jq -r "to_entries|map(\"\(.key)\")|.[]" $ACCOUNT_PATH)
+      for USER in $USERS; do
+        for i in {1..50}; do
+          unset 'Cookie'$i
+        done
+        cd $WEB_PATH &&
+          php -r "require '${WEB_PATH}/functions.php';replaceCookieInShell('${USER}');updateWorkWechatConfig('${USER}');" &&
+          cd /jd
+        FileConf=$TMP_CONFIGFILE
+        Run_Normal $1 $2
+      done
+      cd $WEB_PATH &&
+        php -r "require '${WEB_PATH}/functions.php';getUnBindUsers();updateWorkWechatConfig();" &&
+        cd /jd
+      FileConf=$TMP_CONFIGFILE
       Run_Normal $1 $2
     else
-      echo -e "\n命令输入错误...\n"
-      Help
-    fi
-    ;;
-  3)
-    if [[ $2 == now ]] && [ -f $3 ]; then
-      FileConf=$3
       Run_Normal $1 $2
-    else
-      echo -e "\n临时config文件路径错误...\n"
-      Help
     fi
-    ;;
-  *)
-    echo -e "\n命令过多...\n"
+  else
+    echo -e "\n命令输入错误...\n"
     Help
-    ;;
+  fi
+  ;;
+3)
+  if [[ $2 == now ]] && [ -f $3 ]; then
+    FileConf=$3
+    Run_Normal $1 $2
+  else
+    echo -e "\n临时config文件路径错误...\n"
+    Help
+  fi
+  ;;
+*)
+  echo -e "\n命令过多...\n"
+  Help
+  ;;
 esac
